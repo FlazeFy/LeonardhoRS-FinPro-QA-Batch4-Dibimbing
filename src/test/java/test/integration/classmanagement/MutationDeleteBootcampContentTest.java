1f6ac35983559c02cf8c973935995b8895fd6ab6
FlazeFy2 <79736302+FlazeFy@users.noreply.github.com>
2026-07-14 15:46:20 +0700

    feat(api-test): add API test for TC-CLMG-026 mutation deleteBootcampContent

---
+++ src/test/java/test/integration/classmanagement/MutationDeleteBootcampContentTest.java
@@ -0,0 +1,74 @@
+package test.integration.classmanagement;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import core.BaseApiTest;
+import core.TestDataReader;
+import core.TestUtil;
+
+public class MutationDeleteBootcampContentTest extends BaseApiTest {
+    private static final Logger logger = LogManager.getLogger(MutationUpdateBootcampContentTest.class);
+    private String sid;
+    private String contentId;
+
+    private static final String mutation = """
+     mutation deleteBootcampContent($id: String!) {
+       deleteBootcampContent(id: $id) {
+         id
+       }
+     }
+    """;
+
+    @BeforeMethod
+    public void setUp() {
+        logger.info("Pre-Condition: User already signed in");
+        sid = TestUtil.getSid();
+
+        logger.info("Pre-Condition: At least one content exists");
+        contentId = TestDataReader.getValue("created-content-id");
+
+        // Validate each test data
+        List<Map<String, String>> notEmptyFields = List.of(
+                Map.of("key", "Content Id", "value", contentId)
+        );
+        TestUtil.validateNotEmptyString(notEmptyFields, null);
+    }
+
+    // Positive Test | P2
+    @Test(priority = 4, groups = {"api-test"}, description = "TC-CLMG-026 - User can delete class content")
+    public void deleteBootcampContent() {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("id", contentId);
+
+        // Request
+        Response response = TestUtil.templateGraphQLRequest(
+                "deleteBootcampContent", mutation, variables, config.getProperty("usernameGraphQl"), config.getProperty("passwordGraphQl"), sid
+        );
+        JsonPath jsonPath = response.jsonPath();
+
+        // Validate base structure
+        Assert.assertNotNull(jsonPath.get("data.deleteBootcampContent"));
+        Assert.assertTrue(jsonPath.get("data.deleteBootcampContent") instanceof Map);
+
+        // Validate content props
+        // Get list key / column
+        Map<String, Object> dataObj = jsonPath.getMap("data.deleteBootcampContent");
+
+        List<String> stringFields = List.of("id");
+        TestUtil.validateColumn(dataObj, stringFields, "string", false);
+
+        // Validate each fields not empty / not whitespace only
+        List<String> notEmptyStringFields = List.of("id");
+        TestUtil.validateNotEmptyString(dataObj, notEmptyStringFields);
+
+        logger.info("User can delete class content: executed successfully");
+    }
+}
